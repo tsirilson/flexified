308d4ac76971c8aed7462b12f2ea051e578abf99
David Tsirilson <tsirilson@gmail.com>
2019-12-22 16:19:35 +0200

    refactor(index.ts,styles.scss):better calculation, limited to 34 items

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ class Flexified {
     return [...layoutClasses, itemCountClass]
   }
 
-  private static getOptions(options: object){
+  private static getOptions(options: object) {
     const defaultOptions: Options = {
       itemCount: 0,
       mobileWidthThreshold: 768
@@ -25,63 +25,69 @@ class Flexified {
 
     classList = []
 
-    if(isMobile){
-      classList.push('flexified-mobile')
-
-      if(itemCount % 2 === 0){
-          classList.push('flexified-items-even')
-      }else{
-          classList.push('flexified-items-odd', 'flexified-wide-last-item')
-      }
-
+    // Calculation is supported up to 34 items only
+    if (itemCount < 1 || itemCount > 34) {
       return classList
     }
 
-    if (itemCount == 2) {
+    if (isMobile) {
+      classList.push('flexified-mobile')
+      if (itemCount % 2 === 0) {
+          classList.push('flexified-items-even')
+      }
+      else {
+          classList.push('flexified-items-odd', 'flexified-wide-last-item')
+      }
+      return classList
+    }
+
+    // Number-specific rules
+    if (itemCount == 2 || itemCount == 28) {
       return ['flexified-items-even']
     }
 
-    if (itemCount % 2 !== 0 && itemCount % 3 !== 0) {
-      classList.push('flexified-items-odd')
-      if (itemCount >= 5) {
-          classList.push('flexified-wide-last-item')
-      }
-      return classList
+    if (itemCount == 5 || itemCount == 23|| itemCount == 26 || itemCount == 29) {
+      return ['flexified-items-odd', 'flexified-two-thirds-width-last-item']
     }
 
-    if (itemCount % 3 === 0) {
+    // Non-prime odd number, full width last item
+    if (itemCount == 7 || itemCount % 5 === 0 || itemCount % 22 === 0 || itemCount === 34) {
+      if (itemCount % 3 === 0) {
+        return ['flexified-items-odd']
+      }
+      if (itemCount % 20 === 0) {
+        return ['flexified-items-even']
+      }
+      return ['flexified-items-odd', 'flexified-full-width-last-item']
+    }
+
+    // Non-prime and odd numbers, full width last item
+    if (itemCount == 7 || itemCount % 5 === 0 || itemCount % 22 === 0) {
+      if (itemCount % 3 === 0) {
+        return ['flexified-items-odd']
+      }
+      return ['flexified-items-odd', 'flexified-two-thirds-width-last-item']
+    }
+
+    // Prime number
+    if (this.isPrimeNumber(itemCount)) {
+      if (itemCount % 3 !== 0) {
+        return ['flexified-items-odd', 'flexified-full-width-last-item']
+      }
       return ['flexified-items-odd']
     }
 
-    if (itemCount % 4 === 0) {
-      return ['flexified-items-even']
-    }
-
-    if (itemCount % 10 === 0 && itemCount > 10) {
-      return ['flexified-items-even']
-    }
-
-    if (itemCount % 5 === 0) {
+    // Any odd number, or numbers divided by 6
+    if (itemCount % 2 !== 0 || itemCount % 6 === 0) {
       classList.push('flexified-items-odd')
-      if (itemCount % 10 === 0) {
-          classList.push('flexified-wide-last-item')
-      }
       return classList
     }
 
-    if (itemCount % 7 === 0) {
-      return ['flexified-items-odd', 'flexified-wide-last-item']
+    // Any even number
+    if (itemCount % 2 === 0) {
+      classList.push('flexified-items-even')
+      return classList
     }
-
-    if (itemCount == 22 || itemCount == 26 || itemCount == 34) {
-      return ['flexified-items-odd', 'flexified-wide-last-item']
-    }
-
-    if (itemCount > 2 && itemCount % 2 === 0) {
-      return ['flexified-items-even']
-    }
-
-    return classList
   }
 
   private static isMobile(mobileWidthThreshold: number): boolean{
@@ -92,6 +98,18 @@ class Flexified {
     })
 
     return isMobileWidth
+  }
+
+  private static isPrimeNumber (number: number): boolean {
+    const sqrtnum = Math.floor(Math.sqrt(number))
+    let prime = number != 1
+    for(let i = 2; i < sqrtnum + 1; i++) {
+      if (number % i == 0) {
+        prime = false
+        break
+      }
+    }
+    return prime;
   }
 }
 
